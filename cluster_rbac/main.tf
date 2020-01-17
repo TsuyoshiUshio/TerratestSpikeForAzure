2d059a7a0d92669cdb44edd3cea7f60c07972deb
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-01-16 19:06:58 -0800

    fix: rbac config

--- cluster_rbac/main.tf
+++ cluster_rbac/main.tf
@@ -8,6 +8,9 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     location            = azurerm_resource_group.k8s.location
     resource_group_name = azurerm_resource_group.k8s.name
     dns_prefix          = var.dns_prefix
+    role_based_access_control {
+        enabled = true
+    }
 
     linux_profile {
         admin_username = "ubuntu"
